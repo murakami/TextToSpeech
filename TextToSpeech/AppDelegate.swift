c0eceacb1a5fce221c4603a8fc1dd9ee36de0086
Yukio MURAKAMI <m_yukio@mac.com>
2016-09-15 23:23:09 +0900

    init project

---
+++ TextToSpeech/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextToSpeech
+//
+//  Created by 村上幸雄 on 2016/09/15.
+//  Copyright © 2016年 Yukio MURAKAMI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TextToSpeech/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextToSpeech
+//
+//  Created by 村上幸雄 on 2016/09/15.
+//  Copyright © 2016年 Yukio MURAKAMI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
